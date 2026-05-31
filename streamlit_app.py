e90522396e39bfe2626ab3b742f21050f1c5acac
Fosy-crypto <fosyyy9@gmail.com>
2026-05-31 16:41:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 color = st.color_picker("Pick A Color", "#00FFFF")
 st.write("The current color is", color)
+
+import streamlit as st
+
+st.audio("cat-purr.mp3", format="audio/mpeg", loop=True)
