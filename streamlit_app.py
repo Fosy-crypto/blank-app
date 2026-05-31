5ea8e549d8ca49bdcb1c4806885bb0ec769be87a
Fosy-crypto <fosyyy9@gmail.com>
2026-05-31 16:30:01 +0700

    Remove title and initial instructions from app
    Removed title and introductory text from the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+color = st.color_picker("Pick A Color", "#00FFFF")
+st.write("The current color is", color)
