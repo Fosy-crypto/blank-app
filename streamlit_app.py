acc10bda9a5b6778d6fa0b75556a9e52085ed09d
Fosy-crypto <fosyyy9@gmail.com>
2026-05-31 16:58:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,18 @@ color = st.color_picker("Pick A Color", "#00FFFF")
 st.write("The current color is", color)
 
 import streamlit as st
+your-repository/
+├── pages/
+│   ├── page_1.py
+│   └── page_2.py
+└── your_app.py
+import streamlit as st
+
+if st.button("Home"):
+    st.switch_page("your_app.py")
+if st.button("Page 1"):
+    st.switch_page("pages/page_1.py")
+if st.button("Page 2"):
+    st.switch_page("pages/page_2.py")
 
 st.audio("gemilang_mp3.mp3", format="audio/mpeg", loop=True)
